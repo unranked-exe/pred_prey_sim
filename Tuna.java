e69c40fb2b3d046ad2859d66ef35313c87b6fd60
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-01-30 10:41:01 +0000

    Added Parrotfish and Tuna class
    Added two more prey classes

---
+++ Tuna.java
@@ -0,0 +1,139 @@
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A simple model of a tuna.
+ * tunas age, move, breed, and die.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 7.1
+ */
+public class Tuna extends Animal
+{
+    // Characteristics shared by all tunas (class variables).
+    // The age at which a tuna can start to breed.
+    private static final int BREEDING_AGE = 5;
+    // The age to which a tuna can live.
+    private static final int MAX_AGE = 40;
+    // The likelihood of a tuna breeding.
+    private static final double BREEDING_PROBABILITY = 0.12;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 4;
+    // A shared random number generator to control breeding.
+    private static final Random rand = Randomizer.getRandom();
+    
+    // Individual characteristics (instance fields).
+    
+    // The tuna's age.
+    private int age;
+
+    /**
+     * Create a new tuna. A tuna may be created with age
+     * zero (a new born) or with a random age.
+     * 
+     * @param randomAge If true, the tuna will have a random age.
+     * @param location The location within the field.
+     */
+    public Tuna(boolean randomAge, Location location)
+    {
+        super(location);
+        age = 0;
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+        }
+    }
+    
+    /**
+     * This is what the tuna does most of the time - it runs 
+     * around. Sometimes it will breed or die of old age.
+     * @param currentField The field occupied.
+     * @param nextFieldState The updated field.
+     */
+    public void act(Field currentField, Field nextFieldState)
+    {
+        incrementAge();
+        if(isAlive()) {
+            List<Location> freeLocations = 
+                nextFieldState.getFreeAdjacentLocations(getLocation());
+            if(!freeLocations.isEmpty()) {
+                giveBirth(nextFieldState, freeLocations);
+            }
+            // Try to move into a free location.
+            if(! freeLocations.isEmpty()) {
+                Location nextLocation = freeLocations.get(0);
+                setLocation(nextLocation);
+                nextFieldState.placeAnimal(this, nextLocation);
+            }
+            else {
+                // Overcrowding.
+                setDead();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "tuna{" +
+                "age=" + age +
+                ", alive=" + isAlive() +
+                ", location=" + getLocation() +
+                '}';
+    }
+
+    /**
+     * Increase the age.
+     * This could result in the tuna's death.
+     */
+    private void incrementAge()
+    {
+        age++;
+        if(age > MAX_AGE) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Check whether or not this tuna is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param freeLocations The locations that are free in the current field.
+     */
+    private void giveBirth(Field nextFieldState, List<Location> freeLocations)
+    {
+        // New tunas are born into adjacent locations.
+        // Get a list of adjacent free locations.
+        int births = breed();
+        if(births > 0) {
+            for (int b = 0; b < births && !freeLocations.isEmpty(); b++) {
+                Location loc = freeLocations.remove(0);
+                Tuna young = new Tuna(false, loc);
+                nextFieldState.placeAnimal(young, loc);
+            }
+        }
+    }
+        
+    /**
+     * Generate a number representing the number of births,
+     * if it can breed.
+     * @return The number of births (may be zero).
+     */
+    private int breed()
+    {
+        int births;
+        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        }
+        else {
+            births = 0;
+        }
+        return births;
+    }
+
+    /**
+     * A tuna can breed if it has reached the breeding age.
+     * @return true if the tuna can breed, false otherwise.
+     */
+    private boolean canBreed()
+    {
+        return age >= BREEDING_AGE;
+    }
+}
